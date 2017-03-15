ff7740e9d38b88e89085c99788b4d86fb4fc2e22
Ivan Ivanov <csyntax@outlook.com>
2017-03-15 20:36:58 +0200

    add Models

---
+++ PasteBin/Models/Paste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PasteBin.Models
+{
+    public class Paste
+    {
+        public Paste()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ExpiryDate = DateTime.Now.AddDays(3);
+        }
+
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public bool Private { get; set; }
+
+        public int? LanguageId { get; set; }
+
+        public virtual Language Language { get; set; }
+    }
+}
