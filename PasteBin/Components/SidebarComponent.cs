b7bc21fd4f60931f7bdfa8b587710b57da5d44bc
Ivan Ivanov <csyntax@outlook.com>
2017-03-16 21:47:05 +0200

    sidebar

---
+++ PasteBin/Components/SidebarComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PasteBin.Data.Repositories.Pastes;
+
+namespace PasteBin.Components
+{
+    [ViewComponent(Name = "Sidebar")]
+    public class SidebarComponent : ViewComponent
+    {
+        private readonly IPasteRepository pasteRepository;
+
+        public SidebarComponent(IPasteRepository pasteRepository)
+        {
+            this.pasteRepository = pasteRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var pastes = this.pasteRepository.GetAllAsync();
+
+            return this.View("Sidebar", pastes);
+        }
+    }
+}
